d99fba27d898a77975138987288f866a16dbf2b5
Mark Old <dlom234@gmail.com>
2013-08-20 21:22:53 -0700

    Aero-thin lite (tm)(r)(c)

--- lib/ruush/errors.rb
+++ lib/ruush/errors.rb
@@ -1,32 +1,7 @@
 module Ruush
-  class Errors
-    class Error < StandardError
-      def to_s
-        message
-      end
-    end
-    class BadKey < Error
-      def message
-        "API Key is invalid"
-      end
-    end
-
-    class BadAuth < Error
-      def message
-        "Credentials are invalid"
-      end
-    end
-
-    class BadData < Error
-      def message
-        "Data sent is invalid"
-      end
-    end
-
-    class BadHash < Error
-      def message
-        "Data hash is invalid"
-      end
-    end
-  end
+  class Error < StandardError; end
+  class BadKey < Error; end
+  class BadAuth < Error; end
+  class BadData < Error; end
+  class BadHash < Error; end
 end
